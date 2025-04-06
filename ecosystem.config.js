2f1970f7df8a22bee718ddc88fb07f5cd3c6290a
huangchunhua <huangchunhua@bilibili.com>
2025-04-06 22:17:26 +0800

    feat: add ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,11 +4,9 @@ module.exports = {
       name: 'fun4yeah',
       cwd: 'C:\\Users\\Administrator\\projects\\fun4yeahweb',
       script: 'yarn',
-      args: 'start -- --host 0.0.0.0',
+      args: 'start',
       interpreter: 'cmd.exe',
       interpreterArgs: '/c',
-      exec_mode: 'fork', // 强制单进程模式
-      instances: 1, // 仅启动一个实例
       env: {
         NODE_ENV: 'production',
       },
